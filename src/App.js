f497d815486413ca2fc99a77114a01496f195c3c
Adam Ciołek <adasciolek95@gmail.com>
2023-02-24 14:31:34 +0100

    fixed react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App = () => {
 	return (
 		<>
-			<h1>Hello</h1>
+			<Header />
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+			</Switch>
 		</>
 	);
 };
